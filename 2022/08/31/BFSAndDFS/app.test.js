85441e1d4760b7f040a828fcd94242958063e54d
Woo Dy <kjop7419@naver.com>
2022-08-31 21:51:27 +0900

    :memo: 알고리즘 재귀DFS

--- 2022/08/31/BFSAndDFS/app.test.js
+++ 2022/08/31/BFSAndDFS/app.test.js
@@ -28,6 +28,36 @@ const BFS = (graph, startNode, visited = [], needVisit = [startNode]) => {
   return BFS(graph, startNode, visited, needVisit);
 };
 
+// // while
+// const DFS = (graph, startNode) => {
+//   const visited = [];
+//   let need = [startNode];
+
+//   while (need.length !== 0) {
+//     const node = need.shift();
+//     if (!visited.includes(node)) {
+//       visited.push(node);
+//       need = [...graph[node], ...need];
+//     }
+//   }
+
+//   return visited;
+// };
+
+// 재귀
+const DFS = (graph, startNode, visited = [], need = [startNode]) => {
+  if (need.length === 0) return visited;
+
+  const node = need.shift();
+
+  if (!visited.includes(node)) {
+    visited.push(node);
+    need = [...graph[node], ...need];
+  }
+
+  return DFS(graph, startNode, visited, need);
+};
+
 describe('BFS', () => {
   it('BFS로 탐색한 순서가 나옵니다.', () => {
     expect(
@@ -48,4 +78,22 @@ describe('BFS', () => {
       ),
     ).toEqual(['A', 'B', 'C', 'D', 'G', 'H', 'I', 'E', 'F', 'J']);
   });
+
+  expect(
+    DFS(
+      {
+        A: ['B', 'C'],
+        B: ['A', 'D'],
+        C: ['A', 'G', 'H', 'I'],
+        D: ['B', 'E', 'F'],
+        E: ['D'],
+        F: ['D'],
+        G: ['C'],
+        H: ['C'],
+        I: ['C', 'J'],
+        J: ['I'],
+      },
+      'A',
+    ),
+  ).toEqual(['A', 'B', 'D', 'E', 'F', 'C', 'G', 'H', 'I', 'J']);
 });
